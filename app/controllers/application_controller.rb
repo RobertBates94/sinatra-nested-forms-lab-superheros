522f60feae031775941f375fa0d6286545d432c3
Robert <bates.to.infinity@gmail.com>
2021-08-17 19:16:02 -0500

    commit tree 691945aba4c078a8b4e564beff72b8fb3a3702e2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,19 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-
         erb :super_hero
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+      
+      params[:team][:heroes].each do |details|
+        Heroes.new(details)
+      end
+      
+      @heroes = Heroes.all
+      
+        erb :team
     end
 
 
